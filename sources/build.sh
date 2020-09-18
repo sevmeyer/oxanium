6489d688ef4b98f764be40adcb81b9cc92953828
Denis Moyogo Jacquerye <moyogo@gmail.com>
2020-09-18 15:43:27 +0100

    build.sh: generate VF with varlib, gftools fix-hinting

--- sources/build.sh
+++ sources/build.sh
@@ -14,14 +14,6 @@ echo "Generating Static fonts"
 mkdir -p ../fonts
 fontmake -m Oxanium.designspace -i -o ttf --output-dir ../fonts/ttf/ --keep-overlaps --keep-direction
 
-echo "Generating VFs"
-mkdir -p ../fonts/variable
-fontmake -m Oxanium.designspace -o variable --output-path ../fonts/variable/Oxanium[wght].ttf --keep-overlaps  --keep-direction
-
-
-rm -rf master_ufo/ instance_ufo/ instance_ufos/
-
-
 echo "Post processing"
 ttfs=$(ls ../fonts/ttf/*.ttf)
 for ttf in $ttfs
@@ -48,6 +40,15 @@ do
 	cat hti/Oxanium-All-glyphs.hti >> temp/Oxanium-$weight.hti
 	../tools/htic-ttf.py ../fonts/ttf/Oxanium-$weight.ttf temp/Oxanium-$weight.hti
 done
+
+echo "Generating VFs"
+mkdir -p ../fonts/variable
+# fontmake -m Oxanium.designspace -o variable --output-path ../fonts/variable/Oxanium[wght].ttf --keep-overlaps  --keep-direction
+python3 -m fontTools.varLib Oxanium.designspace --master-finder "../fonts/ttf/{stem}.ttf" -o ../fonts/variable/Oxanium\[wght\].ttf
+
+rm -rf master_ufo/ instance_ufo/ instance_ufos/
+
+
 
 vfs=$(ls ../fonts/variable/*\[wght\].ttf)
 
@@ -80,13 +81,15 @@ done
 echo "Fixing Hinting"
 for vf in $vfs
 do
-	gftools fix-nonhinting $vf "$vf.fix";
+	# gftools fix-nonhinting $vf "$vf.fix";
+	gftools fix-hinting $vf;
 	if [ -f "$vf.fix" ]; then mv "$vf.fix" $vf; fi
 	gftools fix-fsselection $vf --usetypometrics;
 done
 for ttf in $ttfs
 do
 	# gftools fix-nonhinting $ttf "$ttf.fix";
+	gftools fix-hinting $ttf;
     if [ -f "$ttf.fix" ]; then mv "$ttf.fix" $ttf; fi
 	gftools fix-fsselection $ttf --usetypometrics;
 done
