d39faabb7ea75a9014622f9d98671855eef36dcf
Severin Meyer <hello@sev.dev>
2020-09-28 21:46:58 +0200

    Use consistent, recommended formatting in build.sh

--- sources/build.sh
+++ sources/build.sh
@@ -6,17 +6,16 @@ DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 cd "$DIR"
 pwd
 
+
+echo "Generating static fonts"
 rm -rf ../fonts
-
-
-echo "Generating Static fonts"
-mkdir -p ../fonts
+mkdir -p ../fonts/ttf
 fontmake -m Oxanium.designspace -i -o ttf --output-dir ../fonts/ttf/ --keep-overlaps --keep-direction
 
-echo "Post processing"
+
+echo "Post processing static fonts"
 ttfs=(../fonts/ttf/*.ttf)
-for ttf in "${ttfs[@]}"
-do
+for ttf in "${ttfs[@]}"; do
 	gftools fix-dsig -f "$ttf"
 	#ttfautohint "$ttf" "$ttf.fix"
 	#mv "$ttf.fix" "$ttf"
@@ -28,31 +27,26 @@ done
 
 echo "Generating VFs"
 mkdir -p ../fonts/variable
-# fontmake -m Oxanium.designspace -o variable --output-path ../fonts/variable/Oxanium[wght].ttf --keep-overlaps  --keep-direction
+#fontmake -m Oxanium.designspace -o variable --output-path ../fonts/variable/Oxanium[wght].ttf --keep-overlaps  --keep-direction
 python3 -m fontTools.varLib Oxanium.designspace --master-finder "../fonts/ttf/{stem}.ttf" -o ../fonts/variable/Oxanium\[wght\].ttf
-
 rm -rf master_ufo/ instance_ufo/ instance_ufos/
 
 
-
-vfs=(../fonts/variable/*\[wght\].ttf)
-
 echo "Post processing VFs"
-for vf in "${vfs[@]}"
-do
+vfs=(../fonts/variable/*\[wght\].ttf)
+for vf in "${vfs[@]}"; do
 	gftools fix-dsig -f "$vf"
 	#ttfautohint-vf --stem-width-mode nnn "$vf" "$vf.fix"
 	#mv "$vf.fix" "$vf"
 done
 
 
-
-echo "Fix VF Meta"
+echo "Fixing VF meta"
 gftools fix-vf-meta "${vfs[@]}"
 
+
 echo "Dropping MVAR"
-for vf in "${vfs[@]}"
-do
+for vf in "${vfs[@]}"; do
 	mv "$vf.fix" "$vf"
 	ttx -f -x "MVAR" "$vf" # Drop MVAR. Table has issue in DW
 	rtrip="$(basename "$vf" .ttf)"
@@ -60,17 +54,17 @@ do
 	rm "$vf"
 	ttx "$new_file"
 	rm "$new_file"
-
 done
 
-echo "Fixing Hinting"
-for ttf in "${ttfs[@]}" "${vfs[@]}"
-do
-	# gftools fix-nonhinting "$ttf" "$ttf.fix"
+
+echo "Fixing hinting"
+for ttf in "${ttfs[@]}" "${vfs[@]}"; do
+	#gftools fix-nonhinting "$ttf" "$ttf.fix"
 	gftools fix-hinting "$ttf"
-    if [ -f "$ttf.fix" ]; then mv "$ttf.fix" "$ttf"; fi
+	if [ -f "$ttf.fix" ]; then mv "$ttf.fix" "$ttf"; fi
 	gftools fix-fsselection "$ttf" --usetypometrics
 done
+
 
 rm -f ../fonts/variable/*.ttx
 rm -f ../fonts/ttf/*.ttx
